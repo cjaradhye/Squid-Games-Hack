844577909de8e565ec905789e4f063803bcd0105
Harsh Kumar Sinha <hksinha960@gmail.com>
2023-08-20 15:13:38 +0530

    landing started

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Login from "./LoginPage";
-
+import Landing from "./landing";
 
 function App() {
-  return (
-    <Login />
-  );
+  return <Landing />;
 }
 
 export default App;
